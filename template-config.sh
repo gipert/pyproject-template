dd9dc1868040a4d54ac3f2d6caaa12f4c4b47112
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-11-09 13:56:51 +0100

    Address pre-commit complaints

--- template-config.sh
+++ template-config.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-echo -n ">>> GitHub user/organization: "; read ghuser
-echo -n ">>> GitHub repository name: "; read gitrepo
-echo -n ">>> Python package name (leave empty if equal to GitHub repository name): "; read pypkg
+echo -n ">>> GitHub user/organization: "; read -r ghuser
+echo -n ">>> GitHub repository name: "; read -r gitrepo
+echo -n ">>> Python package name (leave empty if equal to GitHub repository name): "; read -r pypkg
 
-[[ ! $pypkg ]] && pypkg=$gitrepo
+[[ ! "$pypkg" ]] && pypkg="$gitrepo"
 
-mv src/pkgplaceholder src/$pypkg
+mv src/pkgplaceholder src/"$pypkg"
 
+# shellcheck disable=SC2046
 sed -i -e "s|ghuserplaceholder|$ghuser|g" \
        -e "s|repoplaceholder|$gitrepo|g" \
        -e "s|pkgplaceholder|$pypkg|g" \
        -- $(find . -type f -not -path "./.git/*")
 
-echo -n "Remove the .git/ folder (recommended)? [y/n] "; read ans
-if [[ "$ans" == "y" ]]; then
+echo -n "Remove the .git/ folder (recommended)? [y/n] "; read -r answer
+if [[ "$answer" == "y" ]]; then
    rm -rf .git
 
    echo -n "Initialize a new Git repository? This will:
@@ -22,10 +23,10 @@ if [[ "$ans" == "y" ]]; then
  - add https://github.com/$ghuser/$gitrepo as remote (origin)
  - create a first commit
 ? [y/n] ";
-   read ans
-   if [[ "$ans" == "y" ]]; then
+   read -r answer
+   if [[ "$answer" == "y" ]]; then
       git init
-      git remote add origin https://github.com/$ghuser/$gitrepo
+      git remote add origin https://github.com/"$ghuser/$gitrepo"
       git add -- . :!"template-config.sh"
       git commit -m "Import and configure pyproject-template"
    fi
